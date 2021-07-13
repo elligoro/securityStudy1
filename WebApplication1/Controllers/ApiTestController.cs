649f14ffcc7d89e259ebe89e6205c20cda5515b1
elligoro <elligoro@gmail.com>
2021-07-13 17:25:18 +0300

    mster start

---
+++ WebApplication1/Controllers/ApiTestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    
+    [ApiController]
+    public class ApiTestController : ControllerBase
+    {
+        [Route("")]
+        public async Task<ActionResult<string>> Get()
+        {
+            return await Task.FromResult(new ActionResult<string>("fuck"));
+        }
+
+    }
+}
